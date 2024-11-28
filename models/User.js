1b21673434a57e466bd361f32be91a41311c190a
EmmanuelCime <ruzdemmanuel@outlook.com>
2024-11-28 12:38:52 -0600

    Revisión de la conexión a DB y modelo User de prueba

---
+++ models/User.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+let collection = "users";
+let schema = new Schema({
+  _id: { type: Number, required: true },
+  photo: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  category: { type: String, enum: ["collaborator", "client"], required: true },
+  itineraries: [{ type: Schema.Types.ObjectId, ref: "Itineraries" }]
+}, {
+    timestamps: true
+});
+
+
+let User = model(collection, schema);
+export default User;
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 import express from "express";
 import "dotenv/config.js";
+import "./config/database.js";
+import cors from "cors";
+import morgan from "morgan";
+import routerIndex from "./router/index.js";
 
 
 const server = express();
 
 const PORT = process.env.PORT || 8080;
 
-const ready = () => {
-    console.log(`Server ready in port :` + PORT);
-};
+const ready = () => console.log(`Server ready in port :` + PORT)
 
-server.listen(PORT, ready);
+
+server.use(express.json()) //Permite trabajar con formato json en entrada y salida
+server.use(express.urlencoded({ extended: true }))
+server.use(cors())
+server.use(morgan('dev')) //Controlar y ver el registro de las peticiones al servidor
+
+//router
+server.use("/api", routerIndex)
+
+
+server.listen(PORT, ready)
