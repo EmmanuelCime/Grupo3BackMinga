b0d0c2156be9df73a099cce935686f17867d3a42
Hidan12 <marcarfe12@gmail.com>
2024-11-29 18:56:14 -0500

    arreglo en router

--- controllers/users/read.js
+++ controllers/users/read.js
@@ -3,7 +3,7 @@ import User from "../../models/User.js";
 let allUsers = async(req, res, next) => {
     try {
         let all = await User.find();
-        return res.status(200).json({res:all})
+        return res.status(200).json({users:all})
     } catch (error) {
         next(error)
     }
--- router/main.js
+++ router/main.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import users from "./users.js"
+import company from "./company.js"
 
 const router = Router();
 
 router.use('/users', users);
+router.use("/company", company)
 
 export default router;
