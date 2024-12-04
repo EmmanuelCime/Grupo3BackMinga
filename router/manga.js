2e037495c513d7f6c5073f7f9bc2c190d4e5b6f0
Hidan12 <marcarfe12@gmail.com>
2024-12-04 16:41:30 -0500

    fix

--- router/manga.js
+++ router/manga.js
@@ -1,21 +1,28 @@
 import { Router } from "express"
-import { allMangas, mangaByCategory } from "../controllers/mangas/read.js"
+import { allMangas, mangaByCategory, mangasByAuthor } from "../controllers/mangas/read.js"
 import { createmanga } from "../controllers/mangas/create.js"
 import { updateManga } from "../controllers/mangas/update.js"
 import { deletedOneManga } from "../controllers/mangas/delete.js"
+import validator from "../middlewares/validator.js"
+import validarorParams from "../middlewares/validator_params.js"
+import schemaCreate from "../schema/manga/create.js"
+import {schemaCategory, schemaAuthor} from "../schema/manga/read.js"
+import schemaUpdate from "../schema/manga/update.js"
+import schemaDelete from "../schema/manga/delete.js"
 
 const router = Router()
 //create
-router.post("/create", createmanga)
+router.post("/create", validator(schemaCreate), createmanga)
 
 //red
 router.get("/all", allMangas)
-router.get("/mangaByCategory/:category", mangaByCategory )
+router.get("/mangaByCategory/:category", validarorParams(schemaCategory), mangaByCategory )
+router.get("/mangasByAuthor/:authorId", validarorParams(schemaAuthor), mangasByAuthor)
 
 //update
-router.put("/update", updateManga)
+router.put("/update", validator(schemaUpdate), updateManga)
 
 //delete
-router.delete("/delete", deletedOneManga)
+router.delete("/delete", validator(schemaDelete), deletedOneManga)
 
 export default router
