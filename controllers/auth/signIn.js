74e3f9b06d8bc4912ac4491fe02a47bbbf1f9fd5
Hidan12 <marcarfe12@gmail.com>
2024-12-06 21:25:59 -0500

    add link front

--- controllers/auth/signIn.js
+++ controllers/auth/signIn.js
@@ -1,19 +1,15 @@
-import User from "../../models/User.js"
+import User from "../../models/User.js";
 
-export default async (req, res, next) => {
+export default async (req,res,next) =>{
     try {
-        await User.findOneAndUpdate({email: req.body.email}, {online: true})
-
-        return res.status(200).json({
-            success: true,
-            message: "Signed In",
-            user:{
-                email: req.body.email,
-                role: req.body.role,
-                photo: req.body.photo
-            }, token: req.token
-        })
+        await User.findOneAndUpdate(
+            {email: req.user.email},
+            {online: true}
+        )
+        return res.redirect("https://mingaapp.netlify.app/?token="+req.token)
     } catch (error) {
         next(error)
     }
 }
+
+
--- models/User.js
+++ models/User.js
@@ -2,11 +2,16 @@ import { Schema, model } from "mongoose";
 
 let collection = 'users';
 
+//rol 0 usuario normal
+//rol 1 usuario author
+//rol 2 usuario compania
+//rol 3 usuario admin
+
 let schema = new Schema({
   email: {type:String, required: true},
   password: {type:String, required: true},
   photo: {type: String, required: true},
-  role:{type:Number},
+  role:{type:Number, default:0},
   online: {type: Boolean}
 }, {
   timeseries:true
